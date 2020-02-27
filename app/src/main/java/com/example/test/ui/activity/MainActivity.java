5eae56567c3d672943f2defa1ee168d8252a2cca
kcj <hui7933114465@163.com>
2020-02-27 15:47:33 +0800

    init

---
+++ app/src/main/java/com/example/test/ui/activity/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.test.ui.activity;
+
+import android.os.Bundle;
+
+import com.example.test.R;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/example/test/widget/xx.java
@@ -0,0 +1,5 @@
+package com.example.test.widget;
+
+public class xx
+{
+}
