7d815668152b5369659b6714cfb716bad6d16126
kcj <hui793114465@163.com>
2020-03-23 10:46:53 +0800

    feat:apt 1.0.7版本

--- module-example-kernel/src/main/java/com/example/kernel/net/api/CommonApi.java
+++ module-example-kernel/src/main/java/com/example/kernel/net/api/CommonApi.java
@@ -14,6 +14,8 @@ import io.reactivex.Single;
 import okhttp3.RequestBody;
 import okhttp3.ResponseBody;
 import retrofit2.http.Body;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
 import retrofit2.http.Headers;
 import retrofit2.http.POST;
@@ -24,11 +26,24 @@ public interface CommonApi {
 
     @Headers("Content-Type:application/json")
     @POST("/manager/user/login")
-    @ApiParams(paramName = {"username", "password"}, request = ApiRequestType.APPLICATIONJSON, response = ApiResponseType.JSONObject)
-    Single<ResponseBody> login123(@Body RequestBody body);
+    @ApiParams(paramName = {"username", "password"}, request = ApiRequestType.APPLICATIONJSON, response = ApiResponseType.JSONArray)
+    Single<ResponseBody> login1(@Body RequestBody body);
 
-    @GET("/manager/product/getListVenue")
-    Single<HttpResult<String>> test2(@Query("username") String username);
+    @Headers("Content-Type:application/json")
+    @FormUrlEncoded
+    @POST("/manager/user/login")
+    @ApiParams(paramName = {"username2", "password1"}, request = ApiRequestType.FORMDATA, response = ApiResponseType.JSONObject)
+    Single<ResponseBody> login2(@Field("username") String username, @Field("password") String password);
+
+
+    @Headers("Content-Type:application/json")
+    @POST("/manager/user/login")
+    @ApiParams(paramName = {"username"}, request = ApiRequestType.APPLICATIONJSON)
+    Single<HttpResult<String>> test1(@Body RequestBody body);
+
+    @Headers("Content-Type:application/json")
+    @POST("/manager/user/login")
+    Single<HttpResult<String>> test2(@Field("username") String username);
 
     @GET("/manager/product/getListVenue")
     Single<HttpResult<HttpListResult<Empty>>> test3(@Query("username") String username);
